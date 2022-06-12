c8cbdcbf5f27c735d5097fdb003f5bcfca466bf4
yuuhi-n <107341115+yuuhi-n@users.noreply.github.com>
2022-06-12 15:39:07 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var display = document.getElementById("output");
+function set(nums) {
+    display.textContent += nums.textContent;
+}
+function calc() {
+    display.textContent = new Function("return " + display.textContent)();
+}
+function reset() {
+    display.textContent = "";
+}
